41649ba5e7bbb5f0399dabcc797ae862223c4365
Murat Şahin <muratsahinco@gmail.com>
2023-05-23 17:54:02 +0300

    Update Program.cs

--- MsMQ/MsMQ/Program.cs
+++ MsMQ/MsMQ/Program.cs
@@ -5,10 +5,15 @@ using Experimental.System.Messaging;
 var testQueue = new MessageQueue().CheckQueue("TestMsMQ");
 testQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
 
-var msg = $"test - {Guid.NewGuid().ToString()}";
-Console.WriteLine(msg);
+for (int i = 0; i < 10; i++)
+{
+    var msg = $"test - {Guid.NewGuid().ToString()}";
+    Console.WriteLine(msg);
 
-//Mesajın queue'ya eklenmesi
-testQueue.Send(msg);
+    //Mesajın queue'ya eklenmesi
+    testQueue.Send(msg);
+    Thread.Sleep(1000);
+}
 
+Console.WriteLine("end");
 Console.ReadLine();
